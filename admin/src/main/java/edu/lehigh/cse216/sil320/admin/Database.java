642bc4799a2f329647d0e6baf4146700f582f755
Kop <sil320@lehigh.edu>
2018-09-18 21:32:32 -0400

    make admin app connected to Heroku server

--- admin/src/main/java/edu/lehigh/cse216/sil320/admin/Database.java
+++ admin/src/main/java/edu/lehigh/cse216/sil320/admin/Database.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import java.util.ArrayList;
 
@@ -102,11 +104,12 @@ public class Database {
      * 
      * @return A Database object, or null if we cannot connect properly
      */
-    static Database getDatabase(String ip, String port, String user, String pass) {
+    static Database getDatabase(String db_url) {
         // Create an un-configured Database object
         Database db = new Database();
-
         // Give the Database object a connection, fail if we cannot get one
+        //old one
+        /*
         try {
             Connection conn = DriverManager.getConnection("jdbc:postgresql://" + ip + ":" + port + "/", user, pass);
             if (conn == null) {
@@ -118,8 +121,32 @@ public class Database {
             System.err.println("Error: DriverManager.getConnection() threw a SQLException");
             e.printStackTrace();
             return null;
-        }
+        }*/
 
+        // Give the Database object a connection, fail if we cannot get one
+        try {
+            Class.forName("org.postgresql.Driver");
+            URI dbUri = new URI(db_url);
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            Connection conn = DriverManager.getConnection(dbUrl, username, password);
+            if (conn == null) {
+                System.err.println("Error: DriverManager.getConnection() returned a null object");
+                return null;
+            }
+            db.mConnection = conn;
+        } catch (SQLException e) {
+            System.err.println("Error: DriverManager.getConnection() threw a SQLException");
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Unable to find postgresql driver");
+            return null;
+        } catch (URISyntaxException s) {
+            System.out.println("URI Syntax Error");
+            return null;
+        }
         // Attempt to create all of our prepared statements.  If any of these 
         // fail, the whole getDatabase() call should fail
         try {
