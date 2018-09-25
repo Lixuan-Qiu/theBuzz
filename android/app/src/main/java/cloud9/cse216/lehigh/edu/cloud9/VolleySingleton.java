f69274011c303389d354a99e175ad83dca9fb10e
Corey Loevinger <cpl220@lehigh.edu>
2018-09-25 04:21:25 -0700

    Phase1 android complete

--- android/app/src/main/java/cloud9/cse216/lehigh/edu/cloud9/VolleySingleton.java
+++ android/app/src/main/java/cloud9/cse216/lehigh/edu/cloud9/VolleySingleton.java
@@ -1,15 +1,26 @@
 package cloud9.cse216.lehigh.edu.cloud9;
 
 import android.content.Context;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 public class VolleySingleton {
     private RequestQueue mRequestQueue;
     private static Context mContext;
+    private static VolleySingleton mInstance;
 
     public VolleySingleton(Context context) {
         mContext = context;
         mRequestQueue = getRequestQueue();
+
+    }
+
+    public VolleySingleton  getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
     }
 
     public RequestQueue getRequestQueue() {
@@ -18,4 +29,10 @@ public class VolleySingleton {
         }
         return mRequestQueue;
     }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
 }
+
+
